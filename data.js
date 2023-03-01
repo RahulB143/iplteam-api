6fd74bfab9049c6934d84c2674bc3cbb789b0254
Rahul Baviskar <113084433+RahulB143@users.noreply.github.com>
2023-03-01 17:12:12 +0530

    ipl api

---
+++ data.js
@@ -0,0 +1,82 @@
+var data = [
+    {
+      "id": "1",
+      "name": "Mumbai Indian-Mi",
+      "preview": "https://static.india.com/wp-content/uploads/2017/03/mumbai-415x246.jpg?impolicy=Medium_Widthonly&w=340",
+      "isAccessory": false,
+      "brand": "Championship Win | 5",
+      "price": "No.of Players | 11"
+    },
+    {
+        "id": "2",
+        "name": "Chennai Super Kings-CSK",
+        "preview": "https://i.pinimg.com/originals/85/52/f8/8552f811e95b998d9505c43a9828c6d6.jpg",
+        "isAccessory": false,
+        "brand": "Championship Win | 4",
+        "price": "No.of Players | 11"
+    },
+    {
+        "id": "3",
+        "name": "Kings XI Punjab-KXIP",
+        "preview": "https://www.celebanything.com/wp-content/uploads/2018/04/Optimized-www.celebanything.com-6-1.jpg",
+        "isAccessory": false,
+        "brand": "Championship Win | 0",
+        "price": "No.of Players | 11"
+    },
+    {
+        "id": "4",
+        "name": "Kolkata Knight Riders-KKR",
+        "preview": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm4MQyuM_PVaAclxV8eGpFng4Zm1yBAOfv3A&usqp=CAU",
+        "isAccessory": false,
+        "brand": "Championship Win | 3",
+        "price": "No.of Players | 11"
+    },
+    {
+        "id": "5",
+        "name": "Royal Challengers Bangalore-RCB",
+        "preview": "https://www.royalchallengers.com/PRRCB01/public/2022-08/cover%20image%20about%20us.png",
+        "isAccessory": false,
+        "brand": "Championship Win | 0",
+        "price": "No.of Players | 11"
+    },
+    {
+        "id": "6",
+        "name": "Rajasthan Royals-RR",
+        "preview": "https://english.cdn.zeenews.com/sites/default/files/styles/zm_700x400/public/2022/02/12/1013863-rr-logo.jpg",
+        "isAccessory": false,
+        "brand": "Championship Win | 1",
+        "price": "No.of Players | 11"
+    },
+    {
+        "id": "7",
+        "name": "Sunrisers Hyderabad-SRH",
+        "preview": "https://i.pinimg.com/736x/b7/7d/f5/b77df59b1a5f0f209b94a0148ddb02d5.jpg",
+        "isAccessory": false,
+        "brand": "Championship Win | 2",
+        "price": "No.of Players | 11"
+    },
+    {
+        "id": "8",
+        "name": "Mumbai Indian-Mi",
+        "preview": "https://static.india.com/wp-content/uploads/2017/03/mumbai-415x246.jpg?impolicy=Medium_Widthonly&w=340",
+        "isAccessory": false,
+        "brand": "Championship Win | 5",
+        "price": "No.of Players | 11"
+    },
+    {
+        "id": "9",
+        "name": "Delhi Capitals-DC",
+        "preview": "https://www.insidesport.in/wp-content/uploads/2019/09/7-3.jpg",
+        "isAccessory": false,
+        "brand": "Championship Win | 0",
+        "price": "No.of Players | 11"
+    },
+    {
+        "id": "10",
+        "name": "Mumbai Indian-Mi",
+        "preview": "https://static.india.com/wp-content/uploads/2017/03/mumbai-415x246.jpg?impolicy=Medium_Widthonly&w=340",
+        "isAccessory": false,
+        "brand": "Championship Win | 5",
+        "price": "No.of Players | 11"
+    }
+  ];
